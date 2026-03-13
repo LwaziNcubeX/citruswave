71e6f4505f4135d9b671cf52515361b7a5878e67
Lwazi Ncube <lwazincubex@gmail.com>
2026-03-13 20:45:56 +0200

    add cart js fuinctionality

---
+++ assets/js/cart.js
@@ -0,0 +1,18 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+    let existingItem = cart.find(item => item.name === name)
+
+    if(existingItem) {
+        existingItem.quantity +=1
+    } else {
+        cart.push({
+            name: name,
+            price: price,
+            quantity: 1
+        })
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+    alert("Added to cart")
+}
